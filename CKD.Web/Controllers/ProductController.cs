180966cc52bea53205a7c96f8b78df6dd087d8b6
Nasser <s.nasser.hadi@gmail.com>
2025-04-29 17:48:03 +0330

    Add Product Controller.

---
+++ CKD.Web/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using CKD.DataAccess.Data;
+using CKD.DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CKD.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProductController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _context.Products.ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
