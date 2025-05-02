8bb575a1c51a38bbfd5c9a74c53001ba01fdb0fb
Nasser <s.nasser.hadi@gmail.com>
2025-05-02 18:32:47 +0330

    Add Product View Medel and product mapping.

--- CKD.Web/Controllers/ProductController.cs
+++ CKD.Web/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using CKD.DataAccess.Data;
 using CKD.DataAccess.Models;
+using CKD.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CKD.Web.Controllers
@@ -15,13 +16,33 @@ namespace CKD.Web.Controllers
         public IActionResult Index()
         {
             IEnumerable<Product> products = _context.Products.ToList();
-            return View(products);
+
+            return View(ToProductViewModels(products));
         }
 
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        public static IEnumerable<ProductViewModel> ToProductViewModels(IEnumerable<Product> inputProducts)
+        {
+            var outputProducts = new List<ProductViewModel>();
+
+            foreach (var inputProduct in inputProducts)
+            {
+                var outputProduct = new ProductViewModel
+                {
+                    Id = inputProduct.ProductCode,
+                    Name = inputProduct.ProductName,
+                    Version = inputProduct.ProductVersion,
+                    Notes = inputProduct.Notes2,
+                    EngineType = inputProduct.EngineTypeDesc
+                };
+                outputProducts.Add(outputProduct);
+            }
+            return outputProducts;
         }
     }
 }
---
+++ CKD.Web/ViewModels/ProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace CKD.Web.ViewModels
+{
+    public class ProductViewModel
+    {
+        [DisplayName("Product Code")]
+        public string Id { get; set; }
+        public int Version { get; set; }
+        [DisplayName("Product Name")]
+        //[Display(Name="Product Name")]
+        public string Name { get; set; }
+        [DisplayName("Desciption")]
+        public string Notes { get; set; }
+        [DisplayName("Engine Type")]
+        public string EngineType { get; set; }
+
+    }
+}
