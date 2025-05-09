a4070cae889eeae9de7bb1287f75a24f5436e2b8
Nasser <s.nasser.hadi@gmail.com>
2025-05-09 19:20:56 +0330

    Add Part Controller and get all Part list.

---
+++ CKD.Web/Controllers/PartController.cs
@@ -0,0 +1,47 @@
+﻿using CKD.DataAccess.Data;
+using CKD.DataAccess.Models;
+using CKD.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CKD.Web.Controllers
+{
+    public class PartController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public PartController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Part> part = _context.Parts.ToList();
+            return View(ToPartViewModels( part));
+        }
+
+        /*_________________________________________________________________________________________________*/
+        /*_________________________________________Helper Methods__________________________________________*/
+        /*_________________________________________________________________________________________________*/
+
+
+        public static IEnumerable<PartViewModel> ToPartViewModels(IEnumerable<Part> inputParts)
+        {
+            var outputParts = new List<PartViewModel>();
+
+            foreach (var inputProduct in inputParts)
+            {
+                var outputPart = new PartViewModel
+                {
+                    Id = inputProduct.TechNo,
+                    Version = inputProduct.Version,
+                    FarsiName = inputProduct.FarsiName,
+                    EnglishName = inputProduct.EnglishName,
+                    IsSet = inputProduct.IsSet,
+                };
+                outputParts.Add(outputPart);
+            }
+            return outputParts;
+        }
+
+    }
+}
---
+++ CKD.Web/ViewModels/PartViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace CKD.Web.ViewModels
+{
+    public class PartViewModel
+    {
+        [DisplayName("TechNo")]
+        public string Id { get; set; } = null!;
+        public int Version { get; set; }
+        [DisplayName("Farsi Name")]
+        public string FarsiName { get; set; } = null!;
+        [DisplayName("English Name")]
+        public string EnglishName { get; set; } = null!;
+        [DisplayName("IsSet")]
+        public bool IsSet { get; set; }
+
+    }
+}
