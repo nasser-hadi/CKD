047a949a44def6b6d16d0e5be2d22ba78449c3f0
Nasser <s.nasser.hadi@gmail.com>
2025-05-12 19:57:32 +0330

    Add Get and Post methods for Create in the Part controller.

--- CKD.Web/Controllers/PartController.cs
+++ CKD.Web/Controllers/PartController.cs
@@ -16,12 +16,12 @@ namespace CKD.Web.Controllers
         public IActionResult Index()
         {
             IEnumerable<Part> part = _context.Parts.ToList();
-            return View(ToPartViewModels( part));
+            return View(ToPartViewModels(part));
         }
 
         /*_________________________________________________________________________________________________*/
         /*_________________________________________Helper Methods__________________________________________*/
-        /*_________________________________________________________________________________________________*/
+        /*________________________________________for mapping data_________________________________________*/
 
 
         public static IEnumerable<PartViewModel> ToPartViewModels(IEnumerable<Part> inputParts)
@@ -42,6 +42,41 @@ namespace CKD.Web.Controllers
             }
             return outputParts;
         }
+        public static Part ToPartDbModel(PartViewModel partVm)
+        {
+            return new Part
+            {
+                TechNo = partVm.Id,
+                Version = partVm.Version,
+                FarsiName = partVm.FarsiName,
+                EnglishName = partVm.EnglishName,
+                IsSet = partVm.IsSet,
+            };
+        }
 
+        /*_________________________________________Create__________________________________________________*/
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(PartViewModel partVm)
+        {
+            if (!ModelState.IsValid)
+            {
+                TempData["Message"] = "There was a problem!";
+                return View();
+            }
+
+                _context.Parts.Add(ToPartDbModel(partVm));
+                await _context.SaveChangesAsync();
+                TempData["Message"] = "New part created successfully.";
+                TempData["success"] = "New part created successfully.";
+                return RedirectToAction(nameof(Index));
+        }
     }
 }
