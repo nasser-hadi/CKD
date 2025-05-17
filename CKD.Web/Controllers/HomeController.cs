604cb20d13d57a970ebe11fca10705829ced6e2b
Nasser <s.nasser.hadi@gmail.com>
2025-05-17 19:11:00 +0330

    Add the About view to the project .

--- CKD.Web/Controllers/HomeController.cs
+++ CKD.Web/Controllers/HomeController.cs
@@ -17,6 +17,10 @@ namespace CKD.Web.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {
